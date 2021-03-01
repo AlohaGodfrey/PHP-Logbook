c164dcb247494b405913c15e24e250b686bee9d7
AlohaGodfrey <60743090+AlohaGodfrey@users.noreply.github.com>
2021-03-01 12:08:08 +0000

    Week 1 Logbook practicals completed

---
+++ Logbooks/Week 1/wk1ex6.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+        $firstname = "Immanuel";
+        $lastname = "Godfrey";
+        $space = " ";
+        $name = $firstname . $space . $lastname;
+        echo $name;
+?>
+</body>
+</html>
